4b172777b99f1a3dd5e13cd09acc759834cb87c4
HinoKsco <ksco@pear.is.utsunomiya-u.ac.jp>
2024-07-17 10:31:43 +0900

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,6 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use tauri::Manager;
 use std::io::{self, Write, Read};
 use serialport::{SerialPortSettings, DataBits, FlowControl, Parity, StopBits};
