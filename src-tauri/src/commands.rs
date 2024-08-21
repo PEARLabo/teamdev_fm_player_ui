24cd9a231162066a5f0a71714b20704dd7908b23
nagaolb <nagaolb@pear.is.utsunomiya-u.ac.jp>
2024-08-21 15:43:42 +0900

    Fix compliation error (and run cargo fmt)

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,12 +1,22 @@
 // src/commands.rs
-use tauri::{Window, State};
-use serialport::{SerialPort, DataBits, Parity, StopBits, FlowControl};
-use std::sync::{Arc, Mutex};
-use magical_global as magical;
 use crate::AppState;
+use magical_global as magical;
+use serialport::{DataBits, FlowControl, Parity, SerialPort, StopBits};
+use std::sync::{Arc, Mutex};
+use tauri::{State, Window};
+
+// magicalクレート側にserialportオブジェクトのサイズを知らせる
+#[cfg(unix)]
+type Port = serialport::TTYPort;
+#[cfg(windows)]
+type Port = serialport::COMPort;
 
 #[tauri::command]
-pub async fn set_serial_port(window: Window, port_name: String, _state: State<'_, Arc<Mutex<AppState>>>) -> Result<(), String> {
+pub async fn set_serial_port(
+    window: Window,
+    port_name: String,
+    _state: State<'_, Arc<Mutex<AppState>>>,
+) -> Result<(), String> {
     let baud_rate = 115200;
 
     let port_setting = serialport::new(port_name.clone(), baud_rate)
@@ -22,23 +32,35 @@ pub async fn set_serial_port(window: Window, port_name: String, _state: State<'_
     if magical::set_at(Box::new(port_setting), 0).is_err() {
         println!("failed to set data");
     }
-    window.emit("playback_info", &format!("Serial port opened: {}", port_name)).unwrap();
+    window
+        .emit(
+            "playback_info",
+            &format!("Serial port opened: {}", port_name),
+        )
+        .unwrap();
 
     Ok(())
 }
 
 #[tauri::command]
-pub async fn disconnect_serial_port(window: Window, _state: State<'_, Arc<Mutex<AppState>>>) -> Result<(), String> {
+pub async fn disconnect_serial_port(
+    window: Window,
+    _state: State<'_, Arc<Mutex<AppState>>>,
+) -> Result<(), String> {
     // グローバルからポート設定を取得して閉じる
     if let Some(port_setting) = magical::get_at_mut::<Box<dyn SerialPort>>(0) {
         // ポートをクローズ
-        port_setting.clear(serialport::ClearBuffer::All).map_err(|e| format!("Failed to clear serial port: {}", e))?;
-        magical::set_at(Box::new(None), 0).map_err(|e| format!("Failed to clear global port setting: {:?}", e))?;
+        port_setting
+            .clear(serialport::ClearBuffer::All)
+            .map_err(|e| format!("Failed to clear serial port: {}", e))?;
+        magical::set_at(Box::new(None::<Port>), 0)
+            .map_err(|e| format!("Failed to clear global port setting: {:?}", e))?;
     } else {
         return Err("No port setting found".into());
     }
 
-    window.emit("playback_info", "Serial port disconnected")
+    window
+        .emit("playback_info", "Serial port disconnected")
         .map_err(|e| format!("Failed to emit playback info: {}", e))?;
 
     Ok(())
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,19 +2,18 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 mod commands;
-use tauri::{Window, State};
-use std::sync::{Arc, Mutex};
-use commands::{set_serial_port, disconnect_serial_port};
+use commands::{disconnect_serial_port, set_serial_port};
 use magical_global as magical;
-use ymodem_send_rs::YmodemSender;
 use serialport::SerialPort;
+use std::sync::{Arc, Mutex};
+use tauri::{State, Window};
+use ymodem_send_rs::YmodemSender;
 //use crate::AppState;
 
 //MISI形式のファイルか判定する関数
 fn check_midi_format(contents: &[u8]) -> bool {
     contents.starts_with(b"MThd")
 }
-
 
 #[derive(Default)]
 struct AppState {
@@ -49,21 +48,28 @@ impl U24 {
     }
 }
 
-
 //ファイルサイズと形式を判定するtauriコマンド
 #[tauri::command]
-fn read_file(contents: Vec<u8>, _state: State<'_, Arc<Mutex<AppState>>>) -> Result<FileInfo, String> {
+fn read_file(
+    contents: Vec<u8>,
+    _state: State<'_, Arc<Mutex<AppState>>>,
+) -> Result<FileInfo, String> {
     println!("Reading file with contents of length: {}", contents.len()); // デバッグ用ログ
 
     let size = contents.len();
-    let is_midi = check_midi_format(&contents); 
+    let is_midi = check_midi_format(&contents);
 
-    Ok(FileInfo { size, is_midi})
+    Ok(FileInfo { size, is_midi })
 }
 
 // ファイルサイズをシリアル通信で送信するTauriコマンド
 #[tauri::command]
-async fn send_file_size<'a>(window: Window, contents: Vec<u8>, _port_name: String, state: State<'_, Arc<Mutex<AppState>>>) -> Result<(), String> {
+async fn send_file_size<'a>(
+    window: Window,
+    contents: Vec<u8>,
+    _port_name: String,
+    state: State<'_, Arc<Mutex<AppState>>>,
+) -> Result<(), String> {
     //ファイルがMIDI形式かどうかを確認
     if !check_midi_format(&contents) {
         //MIDI形式でない場合returnエラー
@@ -73,8 +79,7 @@ async fn send_file_size<'a>(window: Window, contents: Vec<u8>, _port_name: Strin
     // ファイル情報を取得
     let file_info = read_file(contents.clone(), state)?;
     // 既にポートが設定されているか確認
-    let port = magical::get_at::<Box<dyn SerialPort>>(0).unwrap();
-    let mut port = port.as_mut().unwrap(); // `port` をミュータブル参照として取得
+    let mut port = magical::get_at_mut::<Box<dyn SerialPort>>(0).unwrap(); // `port` をミュータブル参照として取得
 
     // ファイルサイズをリトルエンディアンでバイト配列に変換
     let size_bytes = file_info.size.to_le_bytes();
@@ -90,7 +95,9 @@ async fn send_file_size<'a>(window: Window, contents: Vec<u8>, _port_name: Strin
 
     //データ送信が始まったことを知らせるイベント
     println!("Starting send file!");
-    window.emit("playback_info", &"Starting send file!").unwrap();
+    window
+        .emit("playback_info", &"Starting send file!")
+        .unwrap();
 
     // シーケンサからの受信可能メッセージを待機
     let mut response = [0; 1];
@@ -115,8 +122,15 @@ async fn send_file_size<'a>(window: Window, contents: Vec<u8>, _port_name: Strin
                 match port.read_exact(&mut ack) {
                     Ok(_) => {
                         println!("Received ack byte: {:02x}", ack[0]);
-                        window.emit("playback_info", &("Received ack byte: {:02x}", ack[0])).unwrap();
-                        window.emit("playback_info", &("File transfer successful, checksum: {:?}", ack[0])).unwrap();
+                        window
+                            .emit("playback_info", &("Received ack byte: {:02x}", ack[0]))
+                            .unwrap();
+                        window
+                            .emit(
+                                "playback_info",
+                                &("File transfer successful, checksum: {:?}", ack[0]),
+                            )
+                            .unwrap();
 
                         let ack_high_nibble = (ack[0] >> 4) & 0x0F;
                         let ack_low_nibble = ack[0] & 0x0F;
@@ -128,14 +142,16 @@ async fn send_file_size<'a>(window: Window, contents: Vec<u8>, _port_name: Strin
                             // データ転送終了メッセ
                             let ack_message = format!("Received ack byte: {:02x}", ack[0]);
                             println!("{}", ack_message);
-                            window.emit("playback_info", ack_message)
+                            window
+                                .emit("playback_info", ack_message)
                                 .map_err(|e| format!("Failed to emit playback info: {}", e))?;
                             Ok(())
                         } else if ack_low_nibble == 0xC {
                             // 異常終了メッセ
                             port.write_all(&[0xC0])
                                 .map_err(|e| format!("Failed to write to serial port: {}", e))?;
-                            window.emit("playback_info", "Failed to write to serial port")
+                            window
+                                .emit("playback_info", "Failed to write to serial port")
                                 .map_err(|e| format!("Failed to emit playback info: {}", e))?;
                             Err("Incomplete file transfer".into())
                         } else {
@@ -147,7 +163,9 @@ async fn send_file_size<'a>(window: Window, contents: Vec<u8>, _port_name: Strin
                         // タイムアウト後の処理として未完了メッセージを送信する
                         port.write_all(&[0xC0])
                             .map_err(|e| format!("Failed to send incomplete message: {}", e))?;
-                        window.emit("playback_info", &"Failed to read ack from serial port").unwrap();
+                        window
+                            .emit("playback_info", &"Failed to read ack from serial port")
+                            .unwrap();
                         Err("Failed to read ack from serial port".into())
                     }
                 }
@@ -160,49 +178,53 @@ async fn send_file_size<'a>(window: Window, contents: Vec<u8>, _port_name: Strin
             // タイムアウト後の処理として未完了メッセージを送信する
             port.write_all(&[0xC0])
                 .map_err(|e| format!("Failed to send incomplete message: {}", e))?;
-            window.emit("playback_info", "Failed to read from serial port")
+            window
+                .emit("playback_info", "Failed to read from serial port")
                 .map_err(|e| format!("Failed to emit playback info: {}", e))?;
             Err("Failed to read from serial port".into())
         }
     }
 }
 
-
 #[tauri::command]
-async fn process_event(window: Window, _state: State<'_, Arc<Mutex<AppState>>>) -> Result<(), String> {
+async fn process_event(
+    window: Window,
+    _state: State<'_, Arc<Mutex<AppState>>>,
+) -> Result<(), String> {
     // 音楽再生情報を受信するためのバッファ
     let mut buffer = [0; 5]; // 最大5バイトのバッファ
-    // 既にポートが設定されているか確認  
-    let mut port = magical::get_at::<Box<dyn SerialPort>>(0).unwrap();
-    let mut port = port.as_mut().unwrap(); // `port` をミュータブル参照として取得
+                             // 既にポートが設定されているか確認
+    let port = magical::get_at_mut::<Box<dyn SerialPort>>(0).unwrap(); // `port` をミュータブル参照として取得
 
     // フロントへのメッセージ送信デモ
-    window.emit("playback_info", &"Starting playback info").unwrap();
+    window
+        .emit("playback_info", &"Starting playback info")
+        .unwrap();
 
     loop {
         let mut msg_combined = 0u64;
         match port.read_exact(&mut buffer[0..1]) {
-        Ok(_) => {
-            assert!(buffer[0] & 0x0F == 0x01);
-            msg_combined = buffer[0] as u64;
-        },
-        Err(e) => {
-            println!("Failed to read from serial port: {}", e);
-            // エラーメッセージを作成
-            let error_message = ErrorMessage {
-                error: format!("Failed to read from serial port: {}", e),
-            };
+            Ok(_) => {
+                assert!(buffer[0] & 0x0F == 0x01);
+                msg_combined = buffer[0] as u64;
+            }
+            Err(e) => {
+                println!("Failed to read from serial port: {}", e);
+                // エラーメッセージを作成
+                let error_message = ErrorMessage {
+                    error: format!("Failed to read from serial port: {}", e),
+                };
 
-            // JSON形式でフロントエンドにメッセージ送信
-            window.emit("playback_info", &error_message).unwrap();
-        }
+                // JSON形式でフロントエンドにメッセージ送信
+                window.emit("playback_info", &error_message).unwrap();
+            }
         }
         let following_size = (buffer[0] >> 4) & 0xF;
         if following_size == 0 {
-            let flaga_msg = "End".to_string(); 
+            let flaga_msg = "End".to_string();
             println!("{}", flaga_msg);
             window.emit("playback_info", &flaga_msg).unwrap();
-            return Ok(()); 
+            return Ok(());
             //break;
         }
         println!("first byte: {:02X}", buffer[0]);
@@ -214,17 +236,23 @@ async fn process_event(window: Window, _state: State<'_, Arc<Mutex<AppState>>>) 
         match port.read_exact(&mut tmp_buffer) {
             Ok(_) => {
                 for i in 0..(following_size as usize) {
-                buffer[i + 1] = tmp_buffer[i];
-                msg_combined |= (buffer[i + 1] as u64) << ((i + 1) * 8);
+                    buffer[i + 1] = tmp_buffer[i];
+                    msg_combined |= (buffer[i + 1] as u64) << ((i + 1) * 8);
                 }
 
                 // 受信したデータを16進数でログに表示
-                println!("Received playback info (hex): {:02x?}", &buffer[0..(following_size as usize + 1)]);
+                println!(
+                    "Received playback info (hex): {:02x?}",
+                    &buffer[0..(following_size as usize + 1)]
+                );
 
                 let flag_a = u8::from_le((buffer[1]) & 0x0F);
-                let chanel = u8::from_le(buffer[1]>> 4 & 0x0F);
+                let chanel = u8::from_le(buffer[1] >> 4 & 0x0F);
 
-                let tauri_msg = [(msg_combined & 0xFFFFFFFF) as u32, (msg_combined >> 32) as u32];
+                let tauri_msg = [
+                    (msg_combined & 0xFFFFFFFF) as u32,
+                    (msg_combined >> 32) as u32,
+                ];
 
                 //flag_aの判定
                 match flag_a {
@@ -235,13 +263,17 @@ async fn process_event(window: Window, _state: State<'_, Arc<Mutex<AppState>>>) 
                         let velocity = u8::from_le(buffer[3]);
 
                         if velocity == 0 {
-                            let flaga_msg = format!("chanel: {}({:2}), key: {}({:6}), velocity: 0({:11})",
-                                chanel, chanel, key, key, 0);
+                            let flaga_msg = format!(
+                                "chanel: {}({:2}), key: {}({:6}), velocity: 0({:11})",
+                                chanel, chanel, key, key, 0
+                            );
                             println!("{}", flaga_msg);
                             window.emit("playback_info", &tauri_msg).unwrap();
-                        }else if velocity != 0{
-                            let flaga_msg = format!("chanel: {}({:2}), key: {}({:6}), velocity: {}({:11})",
-                                chanel, chanel, key, key, velocity, velocity);
+                        } else if velocity != 0 {
+                            let flaga_msg = format!(
+                                "chanel: {}({:2}), key: {}({:6}), velocity: {}({:11})",
+                                chanel, chanel, key, key, velocity, velocity
+                            );
                             println!("{}", flaga_msg);
                             window.emit("playback_info", &tauri_msg).unwrap();
                         }
@@ -251,14 +283,19 @@ async fn process_event(window: Window, _state: State<'_, Arc<Mutex<AppState>>>) 
                         let tempo = U24::from_be_bytes(buffer[2], buffer[3], buffer[4]);
                         let bpm = 1000000 / tempo.value();
 
-                        let flaga_msg = format!("tempo: {:?}({:?})[μsec/四分音符], BPM: {}", tempo.value(), tempo, bpm);
+                        let flaga_msg = format!(
+                            "tempo: {:?}({:?})[μsec/四分音符], BPM: {}",
+                            tempo.value(),
+                            tempo,
+                            bpm
+                        );
                         println!("{}", flaga_msg);
                         window.emit("playback_info", &tauri_msg).unwrap();
-                    },
+                    }
                     //end event
                     2 => {
                         unreachable!();
-                    },
+                    }
                     //nop event
                     3 => {
                         // No operation
@@ -270,20 +307,44 @@ async fn process_event(window: Window, _state: State<'_, Arc<Mutex<AppState>>>) 
                         let param_data = u8::from_be(buffer[3]);
 
                         let flaga_msg = match event {
-                            0 => format!("Slot: {}({:6}), change param: {}({:11})", slot, slot, param_data, param_data),
-                            1 => format!("Detune/Multiple: {}({:6}), change param: {}({:11})", slot, slot, param_data, param_data),
-                            2 => format!("TotalLevel: {}({:6}), change param: {}({:11})", slot, slot, param_data, param_data),
-                            3 => format!("KeyScale/AttackRate: {}({:6}), change param: {}({:11})", slot, slot, param_data, param_data),
-                            4 => format!("DecayRate: {}({:6}), change param: {}({:11})", slot, slot, param_data, param_data),
-                            5 => format!("SustainRate: {}({:6}), change param: {}({:11})", slot, slot, param_data, param_data),
-                            6 => format!("SustainLevel/ReleaseRate: {}({:6}), change param: {}({:11})", slot, slot, param_data, param_data),
-                            7 => format!("FeedBack/Connection: {}({:6}), change param: {}({:11})", slot, slot, param_data, param_data),
+                            0 => format!(
+                                "Slot: {}({:6}), change param: {}({:11})",
+                                slot, slot, param_data, param_data
+                            ),
+                            1 => format!(
+                                "Detune/Multiple: {}({:6}), change param: {}({:11})",
+                                slot, slot, param_data, param_data
+                            ),
+                            2 => format!(
+                                "TotalLevel: {}({:6}), change param: {}({:11})",
+                                slot, slot, param_data, param_data
+                            ),
+                            3 => format!(
+                                "KeyScale/AttackRate: {}({:6}), change param: {}({:11})",
+                                slot, slot, param_data, param_data
+                            ),
+                            4 => format!(
+                                "DecayRate: {}({:6}), change param: {}({:11})",
+                                slot, slot, param_data, param_data
+                            ),
+                            5 => format!(
+                                "SustainRate: {}({:6}), change param: {}({:11})",
+                                slot, slot, param_data, param_data
+                            ),
+                            6 => format!(
+                                "SustainLevel/ReleaseRate: {}({:6}), change param: {}({:11})",
+                                slot, slot, param_data, param_data
+                            ),
+                            7 => format!(
+                                "FeedBack/Connection: {}({:6}), change param: {}({:11})",
+                                slot, slot, param_data, param_data
+                            ),
                             _ => format!("Invalid event: {}", event),
                         };
 
                         println!("{}", flaga_msg);
                         // window.emit("playback_info", &tauri_msg).unwrap();
-                    },
+                    }
                     5 => {
                         let flaga_msg = "FlagA is 5: Skip to next track.".to_string();
                         println!("{}", flaga_msg);
@@ -309,7 +370,6 @@ async fn process_event(window: Window, _state: State<'_, Arc<Mutex<AppState>>>) 
         }
     }
 }
-
 
 // アプリケーションのエントリーポイント
 fn main() {
