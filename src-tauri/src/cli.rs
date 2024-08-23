492a1033094c1c3efeded4fa3b5850a62d109ec7
PARA-DISO <104207936+PARA-DISO@users.noreply.github.com>
2024-08-23 16:37:17 +0900

    remove magical for cli

--- src-tauri/src/cli.rs
+++ src-tauri/src/cli.rs
@@ -1,11 +1,10 @@
-use crate::{check_midi_format, send_msg, Args, Port};
-use magical_global as magical;
+use crate::{check_midi_format, send_msg, Args};
 use serialport::{DataBits, FlowControl, Parity, StopBits};
 use std::fs::File;
 use std::io::Read;
-
+type Port = Box<dyn serialport::SerialPort>;
 pub fn run(args: Args) {
-    if if let Some(port_name) = args.port_name {
+    let mut port = if let Ok(port) = if let Some(port_name) = args.port_name {
         open_serial_port(port_name)
     } else {
         if let Ok(port_info) = serialport::available_ports() {
@@ -13,11 +12,11 @@ pub fn run(args: Args) {
         } else {
             panic!("No ports");
         }
-    }
-    .is_err()
-    {
+    } {
+        port
+    } else {
         panic!("Could not open port");
-    }
+    };
 
     if let Some(path) = args.input {
         let mut file = File::open(path).unwrap();
@@ -25,28 +24,25 @@ pub fn run(args: Args) {
         file.read_to_end(&mut buf).unwrap();
         if check_midi_format(&buf) {
             println!("Send File Size");
-            send_midi_file(buf);
+            send_midi_file(&mut port, buf);
         } else {
             println!("Not a midi format");
         }
     } else {
         println!("No input path");
     }
-    magical::take_at::<Box<dyn serialport::SerialPort>>(0).unwrap()
 }
 
-fn send_midi_file(buf: Vec<u8>) {
-    let mut port = magical::get_at_mut::<Box<dyn serialport::SerialPort>>(0).unwrap();
-    send_msg::file_size(&mut port, &buf);
+fn send_midi_file(port: &mut Port, buf: Vec<u8>) {
+    send_msg::file_size(port, &buf);
     // Ymodemによるファイル転送(受信可能の場合)
-    let msg_flag = send_msg::receive_byte(&mut port).unwrap() & 0xf;
+    let msg_flag = send_msg::receive_byte(port).unwrap() & 0xf;
     if msg_flag == 0xe {
-        send_msg::file_data(&mut port, &buf);
+        send_msg::file_data(port, &buf);
     } else {
-      magical::take_at::<Box<dyn serialport::SerialPort>>(0).unwrap();
-      panic!("");
+        panic!("");
     }
-    let msg_flag = send_msg::receive_byte(&mut port).unwrap() & 0xf;
+    let msg_flag = send_msg::receive_byte(port).unwrap() & 0xf;
     if msg_flag == 0xc {
         println!("failed to send  midi file");
     } else if msg_flag == 0xd {
@@ -55,7 +51,7 @@ fn send_midi_file(buf: Vec<u8>) {
         unreachable!();
     }
 }
-fn open_serial_port(port: impl AsRef<str>) -> Result<(), String> {
+fn open_serial_port(port: impl AsRef<str>) -> Result<Port, String> {
     let baud_rate = 115200;
     let port_setting = serialport::new(port.as_ref().to_string(), baud_rate)
         .data_bits(DataBits::Eight)
@@ -67,9 +63,6 @@ fn open_serial_port(port: impl AsRef<str>) -> Result<(), String> {
     if port_setting.is_err() {
         return Err("failed to open serial port".to_string());
     }
-    // `magical::set_at` には `Box<dyn SerialPort>` を渡す
-    if magical::set_at(Box::new(port_setting.unwrap()), 0).is_err() {
-        println!("failed to set data");
-    }
-    Ok(())
+
+    Ok(port_setting.unwrap())
 }
