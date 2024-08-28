65da17dfe3b33947deed195eef2ccc8ae6532418
PARA-DISO <104207936+PARA-DISO@users.noreply.github.com>
2024-08-28 11:05:16 +0900

    run js formatter

--- src/player.js
+++ src/player.js
@@ -11,183 +11,194 @@ const BLACK_KEY_WIDTH = WHITE_KEY_WIDTH * 0.6;
 const BLACK_KEY_HEIGHT = WHITE_KEY_HEIGHT * 0.6;
 // ピアノロールの描画(switchPlayer関数内で呼び出し)
 function drawPianoRoll() {
-  const canvas = document.getElementById('pianoRoll');
-  canvas.height = CANVAS_HEIGHT; // Canvasの高さを設定
-  canvas.width = CANVAS_WIDTH; // Canvasの幅を設定
+	const canvas = document.getElementById("pianoRoll");
+	canvas.height = CANVAS_HEIGHT; // Canvasの高さを設定
+	canvas.width = CANVAS_WIDTH; // Canvasの幅を設定
 
-  const ctx = canvas.getContext('2d');
-  
-  const numOctaves = 6; // 6オクターブ表示に調整
+	const ctx = canvas.getContext("2d");
 
-  const whiteKeys = [0, 2, 4, 5, 7, 9, 11];
-  const blackKeys = [1, 3, 6, 8, 10];
-  const module_3_table = [0,1,2,0];
-  let startTime = null;
+	const numOctaves = 6; // 6オクターブ表示に調整
 
-  // ピアノの描画
-  function drawPiano() {
-    // 先に全ての白鍵を描画
-    for (let octave = 0; octave < numOctaves; octave++) {
-      const octaveOffsetX = module_3_table[(octave % 3)] * 7 * WHITE_KEY_WIDTH;
-      const yOffset = Math.floor(octave / 3) * PIANO_HEIGHT;
-      // Note: 破壊的な変更(復帰可能にするためにコメントで保護)
-      for(let i = 0,_end = whiteKeys.length; i < _end; i++) {
-        let key = whiteKeys[i];
-        const pitch = octave * 12 + key + 24; // C1から開始するように24を追加
-        const x = octaveOffsetX + i * WHITE_KEY_WIDTH;
-        const y = yOffset;
-        ctx.fillStyle = activeNotes.has(pitch) ? '#D3D3D3' : 'white';  // 薄い灰色でアクティブな白鍵を表示
-        ctx.fillRect(x, y, WHITE_KEY_WIDTH, WHITE_KEY_HEIGHT);
-        ctx.strokeStyle = 'black';
-        ctx.strokeRect(x, y, WHITE_KEY_WIDTH, WHITE_KEY_HEIGHT);
+	const whiteKeys = [0, 2, 4, 5, 7, 9, 11];
+	const blackKeys = [1, 3, 6, 8, 10];
+	const module_3_table = [0, 1, 2, 0];
+	let startTime = null;
 
-        // Cのラベルを表示
-        if (key === 0) {
-          ctx.fillStyle = 'black';
-          ctx.font = '14px Arial';
-          ctx.fillText(`C${Math.floor(pitch / 12) - 1}`, x + 5, y + WHITE_KEY_HEIGHT - 5);
-        }
-      }
-      // whiteKeys.forEach((key, i) => {
-      //   const pitch = octave * 12 + key + 24; // C1から開始するように24を追加
-      //   const x = octaveOffsetX + i * WHITE_KEY_WIDTH;
-      //   const y = yOffset;
-      //   ctx.fillStyle = activeNotes.has(pitch) ? '#D3D3D3' : 'white';  // 薄い灰色でアクティブな白鍵を表示
-      //   ctx.fillRect(x, y, WHITE_KEY_WIDTH, WHITE_KEY_HEIGHT);
-      //   ctx.strokeStyle = 'black';
-      //   ctx.strokeRect(x, y, WHITE_KEY_WIDTH, WHITE_KEY_HEIGHT);
+	// ピアノの描画
+	function drawPiano() {
+		// 先に全ての白鍵を描画
+		for (let octave = 0; octave < numOctaves; octave++) {
+			const octaveOffsetX = module_3_table[octave % 3] * 7 * WHITE_KEY_WIDTH;
+			const yOffset = Math.floor(octave / 3) * PIANO_HEIGHT;
+			// Note: 破壊的な変更(復帰可能にするためにコメントで保護)
+			for (let i = 0, _end = whiteKeys.length; i < _end; i++) {
+				let key = whiteKeys[i];
+				const pitch = octave * 12 + key + 24; // C1から開始するように24を追加
+				const x = octaveOffsetX + i * WHITE_KEY_WIDTH;
+				const y = yOffset;
+				ctx.fillStyle = activeNotes.has(pitch) ? "#D3D3D3" : "white"; // 薄い灰色でアクティブな白鍵を表示
+				ctx.fillRect(x, y, WHITE_KEY_WIDTH, WHITE_KEY_HEIGHT);
+				ctx.strokeStyle = "black";
+				ctx.strokeRect(x, y, WHITE_KEY_WIDTH, WHITE_KEY_HEIGHT);
 
-      //   // Cのラベルを表示
-      //   if (key === 0) {
-      //     ctx.fillStyle = 'black';
-      //     ctx.font = '14px Arial';
-      //     ctx.fillText(`C${Math.floor(pitch / 12) - 1}`, x + 5, y + WHITE_KEY_HEIGHT - 5);
-      //   }
-      // });
-    }
+				// Cのラベルを表示
+				if (key === 0) {
+					ctx.fillStyle = "black";
+					ctx.font = "14px Arial";
+					ctx.fillText(
+						`C${Math.floor(pitch / 12) - 1}`,
+						x + 5,
+						y + WHITE_KEY_HEIGHT - 5,
+					);
+				}
+			}
+			// whiteKeys.forEach((key, i) => {
+			//   const pitch = octave * 12 + key + 24; // C1から開始するように24を追加
+			//   const x = octaveOffsetX + i * WHITE_KEY_WIDTH;
+			//   const y = yOffset;
+			//   ctx.fillStyle = activeNotes.has(pitch) ? '#D3D3D3' : 'white';  // 薄い灰色でアクティブな白鍵を表示
+			//   ctx.fillRect(x, y, WHITE_KEY_WIDTH, WHITE_KEY_HEIGHT);
+			//   ctx.strokeStyle = 'black';
+			//   ctx.strokeRect(x, y, WHITE_KEY_WIDTH, WHITE_KEY_HEIGHT);
 
-    // 黒鍵を描画し、その上にアクティブな色を適用
-    for (let octave = 0; octave < numOctaves; octave++) {
-      const octaveOffsetX = module_3_table[(octave % 3)] * 7 * WHITE_KEY_WIDTH;
-      const yOffset = Math.floor(octave / 3) * PIANO_HEIGHT;
-      // Note: 破壊的な変更(復帰可能にするためにコメントで保護)
-      for(let i = 0,_end = blackKeys.length; i < _end; i++) {
-        let key = blackKeys[i];
-        const pitch = octave * 12 + key + 24; // C#1から開始するように24を追加
-        const x = octaveOffsetX + whiteKeys.indexOf(key - 1) * WHITE_KEY_WIDTH + WHITE_KEY_WIDTH - BLACK_KEY_WIDTH / 2;
-        const y = yOffset;
-        // 黒鍵の基本描画
-        ctx.fillStyle = activeNotes.has(pitch) ? '#A9A9A9' : 'black';  // 濃い灰色でアクティブな黒鍵を表示
-        ctx.fillRect(x, y, BLACK_KEY_WIDTH, BLACK_KEY_HEIGHT);
-        ctx.strokeStyle = 'black';
-        ctx.strokeRect(x, y, BLACK_KEY_WIDTH, BLACK_KEY_HEIGHT);
-      }
-      // blackKeys.forEach((key) => {
-      //   const pitch = octave * 12 + key + 24; // C#1から開始するように24を追加
-      //   const x = octaveOffsetX + whiteKeys.indexOf(key - 1) * WHITE_KEY_WIDTH + WHITE_KEY_WIDTH - BLACK_KEY_WIDTH / 2;
-      //   const y = yOffset;
+			//   // Cのラベルを表示
+			//   if (key === 0) {
+			//     ctx.fillStyle = 'black';
+			//     ctx.font = '14px Arial';
+			//     ctx.fillText(`C${Math.floor(pitch / 12) - 1}`, x + 5, y + WHITE_KEY_HEIGHT - 5);
+			//   }
+			// });
+		}
 
-      //   // 黒鍵の基本描画
-      //   ctx.fillStyle = activeNotes.has(pitch) ? '#A9A9A9' : 'black';  // 濃い灰色でアクティブな黒鍵を表示
-      //   ctx.fillRect(x, y, BLACK_KEY_WIDTH, BLACK_KEY_HEIGHT);
-      //   ctx.strokeStyle = 'black';
-      //   ctx.strokeRect(x, y, BLACK_KEY_WIDTH, BLACK_KEY_HEIGHT);
-      // });
-    }
-  }
+		// 黒鍵を描画し、その上にアクティブな色を適用
+		for (let octave = 0; octave < numOctaves; octave++) {
+			const octaveOffsetX = module_3_table[octave % 3] * 7 * WHITE_KEY_WIDTH;
+			const yOffset = Math.floor(octave / 3) * PIANO_HEIGHT;
+			// Note: 破壊的な変更(復帰可能にするためにコメントで保護)
+			for (let i = 0, _end = blackKeys.length; i < _end; i++) {
+				let key = blackKeys[i];
+				const pitch = octave * 12 + key + 24; // C#1から開始するように24を追加
+				const x =
+					octaveOffsetX +
+					whiteKeys.indexOf(key - 1) * WHITE_KEY_WIDTH +
+					WHITE_KEY_WIDTH -
+					BLACK_KEY_WIDTH / 2;
+				const y = yOffset;
+				// 黒鍵の基本描画
+				ctx.fillStyle = activeNotes.has(pitch) ? "#A9A9A9" : "black"; // 濃い灰色でアクティブな黒鍵を表示
+				ctx.fillRect(x, y, BLACK_KEY_WIDTH, BLACK_KEY_HEIGHT);
+				ctx.strokeStyle = "black";
+				ctx.strokeRect(x, y, BLACK_KEY_WIDTH, BLACK_KEY_HEIGHT);
+			}
+			// blackKeys.forEach((key) => {
+			//   const pitch = octave * 12 + key + 24; // C#1から開始するように24を追加
+			//   const x = octaveOffsetX + whiteKeys.indexOf(key - 1) * WHITE_KEY_WIDTH + WHITE_KEY_WIDTH - BLACK_KEY_WIDTH / 2;
+			//   const y = yOffset;
 
-  // 経過時間の表示
-  function displayTime(elapsedTime) {
-    ctx.fillStyle = 'black';
-    ctx.font = '16px Arial';
-    // 秒数は小数点以下1桁まで表示
-    ctx.fillText(`Time: ${Math.floor(elapsedTime / 1000)}s`, 10, canvas.height - 5);
-  }
+			//   // 黒鍵の基本描画
+			//   ctx.fillStyle = activeNotes.has(pitch) ? '#A9A9A9' : 'black';  // 濃い灰色でアクティブな黒鍵を表示
+			//   ctx.fillRect(x, y, BLACK_KEY_WIDTH, BLACK_KEY_HEIGHT);
+			//   ctx.strokeStyle = 'black';
+			//   ctx.strokeRect(x, y, BLACK_KEY_WIDTH, BLACK_KEY_HEIGHT);
+			// });
+		}
+	}
 
-  // アニメーションの開始
-  function animate(time) {
-    if (!startTime) startTime = time;
-    const elapsedTime = time - startTime;
+	// 経過時間の表示
+	function displayTime(elapsedTime) {
+		ctx.fillStyle = "black";
+		ctx.font = "16px Arial";
+		// 秒数は小数点以下1桁まで表示
+		ctx.fillText(
+			`Time: ${Math.floor(elapsedTime / 1000)}s`,
+			10,
+			canvas.height - 5,
+		);
+	}
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawPiano();
-    displayTime(elapsedTime);
+	// アニメーションの開始
+	function animate(time) {
+		if (!startTime) startTime = time;
+		const elapsedTime = time - startTime;
 
-    processEventQueue();
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		drawPiano();
+		displayTime(elapsedTime);
 
-    requestAnimationFrame(animate);
-  }
+		processEventQueue();
 
-  requestAnimationFrame(animate);
+		requestAnimationFrame(animate);
+	}
 
-  // ノートオンイベントを処理
-  function noteOn(pitch) {
-    if (activeNotes.has(pitch)) return;
-    activeNotes.add(pitch);
-  }
+	requestAnimationFrame(animate);
 
-  // ノートオフイベントを処理
-  function noteOff(pitch) {
-    activeNotes.delete(pitch);
-    if (activeNotes.has(pitch)) {
-      noteOff(pitch);
-    }
-  }
+	// ノートオンイベントを処理
+	function noteOn(pitch) {
+		if (activeNotes.has(pitch)) return;
+		activeNotes.add(pitch);
+	}
 
-  // 外部からノートオン、ノートオフをトリガーできるようにする
-  window.noteOn = noteOn;
-  window.noteOff = noteOff;
+	// ノートオフイベントを処理
+	function noteOff(pitch) {
+		activeNotes.delete(pitch);
+		if (activeNotes.has(pitch)) {
+			noteOff(pitch);
+		}
+	}
+
+	// 外部からノートオン、ノートオフをトリガーできるようにする
+	window.noteOn = noteOn;
+	window.noteOff = noteOff;
 }
 
 // イベントキューの処理
 function processEventQueue() {
-  while (eventQueue.length > 0) {
-    const data = eventQueue.dequeue();
-    handleEvent(data);
-  }
+	while (eventQueue.length > 0) {
+		const data = eventQueue.dequeue();
+		handleEvent(data);
+	}
 }
 
 // イベントデータをキューに追加
 function updatePianoRoll(data) {
-  eventQueue.enqueue(data);
-  const playerConsoleArea = document.getElementById('playerConsole');
-  if (playerConsoleArea) {
-    playerConsoleArea.value = `Playback Data: key: ${data.key}, vel: ${data.vel}\n`;
-    playerConsoleArea.scrollTop = playerConsoleArea.scrollHeight;
-  }
+	eventQueue.enqueue(data);
+	const playerConsoleArea = document.getElementById("playerConsole");
+	if (playerConsoleArea) {
+		playerConsoleArea.value = `Playback Data: key: ${data.key}, vel: ${data.vel}\n`;
+		playerConsoleArea.scrollTop = playerConsoleArea.scrollHeight;
+	}
 }
 
 // イベントデータの処理
 function handleEvent(data) {
-  const playerConsoleArea = document.getElementById('playerConsole');
-  // playerConsoleArea.value += `Playback Data: key: ${data.key}, vel: ${data.vel}\n`;
-  playerConsoleArea.scrollTop = playerConsoleArea.scrollHeight;
-  if(data.vel !== 0) {
-    // console.log(`DATA | vel: ${data.vel}, key: ${data.key}\n`)
-    window.noteOn(data.key);
-  } else if(data.vel == 0) {
-    // console.log(`DATA | vel: ${data.vel}, key: ${data.key}\n`)
-    window.noteOff(data.key);
-  } else {
-    // その他のイベントを処理
-    console.warn("Unknown event type:", data);
-  }
+	const playerConsoleArea = document.getElementById("playerConsole");
+	// playerConsoleArea.value += `Playback Data: key: ${data.key}, vel: ${data.vel}\n`;
+	playerConsoleArea.scrollTop = playerConsoleArea.scrollHeight;
+	if (data.vel !== 0) {
+		// console.log(`DATA | vel: ${data.vel}, key: ${data.key}\n`)
+		window.noteOn(data.key);
+	} else if (data.vel == 0) {
+		// console.log(`DATA | vel: ${data.vel}, key: ${data.key}\n`)
+		window.noteOff(data.key);
+	} else {
+		// その他のイベントを処理
+		console.warn("Unknown event type:", data);
+	}
 }
 
 function updateTempo(data) {
-  // テンポ情報をパースして更新
-  // const tempoMatch = data.match(/tempo: (\d+)/);
-  if (data.tempo) {
-    // const tempo = parseInt(tempoMatch[1], 10);
-    const tempoDisplay = document.getElementById('tempoDisplay');
-    if (tempoDisplay) {
-      tempoDisplay.textContent = `Tempo: ${data.tempo}`;
-    }
-  }
+	// テンポ情報をパースして更新
+	// const tempoMatch = data.match(/tempo: (\d+)/);
+	if (data.tempo) {
+		// const tempo = parseInt(tempoMatch[1], 10);
+		const tempoDisplay = document.getElementById("tempoDisplay");
+		if (tempoDisplay) {
+			tempoDisplay.textContent = `Tempo: ${data.tempo}`;
+		}
+	}
 }
 
 function handleEndEvent() {
-  activeNotes.clear();
-  console.log("End Event received.");
+	activeNotes.clear();
+	console.log("End Event received.");
 }
-
--- src/queue.js
+++ src/queue.js
@@ -1,147 +1,140 @@
 class Queue {
-  // private buffer_capacity: number
-  constructor(capacity) {
-      this.start = 0;
-      this.count = 0;
-      // bufferサイズの制限(上限ではない)
-      this.capacity = 512;
-      if (capacity) {
-          this.buffer = new Array(capacity);
-          this.capacity = capacity;
-      }
-      else {
-          this.buffer = new Array();
-      }
-  }
-  at(index) {
-      if (index < 0) {
-          let t = index + this.count;
-          if (t < 0) {
-              return undefined;
-          }
-          else {
-              return this.buffer[index + this.start + this.count];
-          }
-      }
-      else {
-          return this.buffer[index + this.start];
-      }
-  }
-  first() {
-      return this.buffer[this.start];
-  }
-  last() {
-      return this.buffer[this.count + this.start];
-  }
-  /* 操作系メソッド */
-  change_at(idx, value) {
-      if (idx < 0) {
-          let t = idx + this.count;
-          if (t >= 0) {
-              this.buffer[idx + this.start + this.count] = value;
-          }
-      }
-      else {
-          this.buffer[idx + this.start] = value;
-      }
-  }
-  change_at_by(idx, f) {
-      let index;
-      if (idx < 0) {
-          index = idx + this.count;
-          if (index < 0) {
-              index = this.start;
-          }
-          else {
-              index += this.start;
-          }
-      }
-      else {
-          index = this.start + idx;
-      }
-      let v = this.buffer[index];
-      if (v) {
-          this.buffer[index] = f(v);
-      }
-  }
-  change_last(value) {
-      this.buffer[this.start + this.count] = value;
-  }
-  change_last_by(f) {
-      let v = this.buffer[this.start + this.count];
-      if (v) {
-          this.buffer[this.start + this.count] = f(v);
-      }
-  }
-  /**
-   * @description
-   * @param {T} value
-   */
-  enqueue(value) {
-      if (this.start + this.count + 1 > this.capacity) {
-          this.shrink_to_fit();
-      }
-      this.buffer[this.start + this.count++] = value;
-  }
-  /**
-   * @description
-   * @returns {T|undefined}
-   */
-  dequeue() {
-      if (this.count) {
-          let item = this.buffer[this.start];
-          this.buffer[this.start++] = undefined;
-          this.count--;
-          return item;
-      }
-      else {
-          return undefined;
-      }
-  }
-  /**
-   * @description
-   * @param {Array<T>}values
-   */
-  append(values) {
-      let l = values.length;
-      // データをシフト
-      if (this.start + this.count + l > this.capacity) {
-          this.shrink_to_fit();
-      }
-      // Queueに追加
-      let offset = this.start + this.count;
-      for (let i = 0; i < l; i++) {
-          this.buffer[i + offset] = values[i];
-      }
-      this.count += l;
-  }
-  /**
-   * @description
-   */
-  shrink_to_fit() {
-      if (this.start) {
-          let buf = this.buffer;
-          let n = this.count < this.buffer.length ? this.count : this.buffer.length;
-          let offset = this.start;
-          for (let i = 0; i < n; i++) {
-              buf[i] = buf[i + offset];
-          }
-          this.buffer.length = n > this.capacity ? n : this.capacity;
-          this.start = 0;
-      }
-  }
-  /**
-   * @description Clears the Queue, removing all values.
-   */
-  clear() {
-      this.start = 0;
-      this.count = 0;
-      for (let i = this.start; i < this.count; i++) {
-          this.buffer[i] = undefined;
-      }
-      this.buffer.length = this.capacity;
-  }
-  get length() {
-      return this.count;
-  }
+	// private buffer_capacity: number
+	constructor(capacity) {
+		this.start = 0;
+		this.count = 0;
+		// bufferサイズの制限(上限ではない)
+		this.capacity = 512;
+		if (capacity) {
+			this.buffer = new Array(capacity);
+			this.capacity = capacity;
+		} else {
+			this.buffer = new Array();
+		}
+	}
+	at(index) {
+		if (index < 0) {
+			let t = index + this.count;
+			if (t < 0) {
+				return undefined;
+			} else {
+				return this.buffer[index + this.start + this.count];
+			}
+		} else {
+			return this.buffer[index + this.start];
+		}
+	}
+	first() {
+		return this.buffer[this.start];
+	}
+	last() {
+		return this.buffer[this.count + this.start];
+	}
+	/* 操作系メソッド */
+	change_at(idx, value) {
+		if (idx < 0) {
+			let t = idx + this.count;
+			if (t >= 0) {
+				this.buffer[idx + this.start + this.count] = value;
+			}
+		} else {
+			this.buffer[idx + this.start] = value;
+		}
+	}
+	change_at_by(idx, f) {
+		let index;
+		if (idx < 0) {
+			index = idx + this.count;
+			if (index < 0) {
+				index = this.start;
+			} else {
+				index += this.start;
+			}
+		} else {
+			index = this.start + idx;
+		}
+		let v = this.buffer[index];
+		if (v) {
+			this.buffer[index] = f(v);
+		}
+	}
+	change_last(value) {
+		this.buffer[this.start + this.count] = value;
+	}
+	change_last_by(f) {
+		let v = this.buffer[this.start + this.count];
+		if (v) {
+			this.buffer[this.start + this.count] = f(v);
+		}
+	}
+	/**
+	 * @description
+	 * @param {T} value
+	 */
+	enqueue(value) {
+		if (this.start + this.count + 1 > this.capacity) {
+			this.shrink_to_fit();
+		}
+		this.buffer[this.start + this.count++] = value;
+	}
+	/**
+	 * @description
+	 * @returns {T|undefined}
+	 */
+	dequeue() {
+		if (this.count) {
+			let item = this.buffer[this.start];
+			this.buffer[this.start++] = undefined;
+			this.count--;
+			return item;
+		} else {
+			return undefined;
+		}
+	}
+	/**
+	 * @description
+	 * @param {Array<T>}values
+	 */
+	append(values) {
+		let l = values.length;
+		// データをシフト
+		if (this.start + this.count + l > this.capacity) {
+			this.shrink_to_fit();
+		}
+		// Queueに追加
+		let offset = this.start + this.count;
+		for (let i = 0; i < l; i++) {
+			this.buffer[i + offset] = values[i];
+		}
+		this.count += l;
+	}
+	/**
+	 * @description
+	 */
+	shrink_to_fit() {
+		if (this.start) {
+			let buf = this.buffer;
+			let n = this.count < this.buffer.length ? this.count : this.buffer.length;
+			let offset = this.start;
+			for (let i = 0; i < n; i++) {
+				buf[i] = buf[i + offset];
+			}
+			this.buffer.length = n > this.capacity ? n : this.capacity;
+			this.start = 0;
+		}
+	}
+	/**
+	 * @description Clears the Queue, removing all values.
+	 */
+	clear() {
+		this.start = 0;
+		this.count = 0;
+		for (let i = this.start; i < this.count; i++) {
+			this.buffer[i] = undefined;
+		}
+		this.buffer.length = this.capacity;
+	}
+	get length() {
+		return this.count;
+	}
 }
