6930cd7d09710de008f7cdba56aa4acf4157d963
PARA-DISO <104207936+PARA-DISO@users.noreply.github.com>
2024-08-28 11:10:42 +0900

    change formatter indent style to soft tab from hard tab

--- src/main.js
+++ src/main.js
@@ -10,100 +10,101 @@ let tauriFunctionName = "send_file_size"; // 本番用
 
 // div.playerを表示し、div.mainを非表示にする関数
 function switchPlayer() {
-	const main = document.getElementById("main");
-	const player = document.getElementById("player");
-	main.style.display = "none";
-	player.style.display = "block";
-	// ピアノロールの描画
-	drawPianoRoll();
-	// #console をクリア
-	document.getElementById("console").value = null;
-	document.getElementById("playerConsole").value = null;
+  const main = document.getElementById("main");
+  const player = document.getElementById("player");
+  main.style.display = "none";
+  player.style.display = "block";
+  // ピアノロールの描画
+  drawPianoRoll();
+  // #console をクリア
+  document.getElementById("console").value = null;
+  document.getElementById("playerConsole").value = null;
 }
 
 // div.mainを表示し、div.playerを非表示にする関数
 function switchMain() {
-	const main = document.getElementById("main");
-	const player = document.getElementById("player");
-	main.style.display = "block";
-	player.style.display = "none";
-	// #console をクリア
-	document.getElementById("console").value = null;
-	document.getElementById("playerConsole").value = null;
+  const main = document.getElementById("main");
+  const player = document.getElementById("player");
+  main.style.display = "block";
+  player.style.display = "none";
+  // #console をクリア
+  document.getElementById("console").value = null;
+  document.getElementById("playerConsole").value = null;
 
-	// イベントリスナを解除
-	if (playbackListenerId) {
-		playbackListenerId();
-		playbackListenerId = null;
-	}
+  // イベントリスナを解除
+  if (playbackListenerId) {
+    playbackListenerId();
+    playbackListenerId = null;
+  }
 }
 
 // シリアルポート設定ボタンのクリックイベントリスナーを追加
 document
-	.getElementById("setSerialPortButton")
-	.addEventListener("click", async () => {
-		const serialPortInput = document.getElementById("serialPortInput").value;
-		if (serialPortInput) {
-			portName = serialPortInput; // ポート名を更新
-			await invoke("set_serial_port", { portName: serialPortInput });
-			console.log(`Serial port set to: ${portName}`); // デバッグ用ログ
-		} else {
-			console.error("Invalid serial port input.");
-		}
-	});
+  .getElementById("setSerialPortButton")
+  .addEventListener("click", async () => {
+    const serialPortInput = document.getElementById("serialPortInput").value;
+    if (serialPortInput) {
+      portName = serialPortInput; // ポート名を更新
+      await invoke("set_serial_port", { portName: serialPortInput });
+      console.log(`Serial port set to: ${portName}`); // デバッグ用ログ
+    } else {
+      console.error("Invalid serial port input.");
+    }
+  });
 
 // Disconnectボタンのクリックイベントリスナーを追加
 document
-	.getElementById("disconnectButton")
-	.addEventListener("click", async () => {
-		try {
-			// Rust側のdisconnect関数を呼び出す
-			await invoke("disconnect_serial_port");
-			console.log("Serial port disconnected successfully");
-			const consoleArea = document.getElementById("console");
-			consoleArea.value += "Serial port disconnected successfully\n";
-		} catch (error) {
-			console.error(`Error during disconnect: ${error}`);
-			const consoleArea = document.getElementById("console");
-			consoleArea.value += `Error during disconnect: ${error}\n`;
-		}
-	});
+  .getElementById("disconnectButton")
+  .addEventListener("click", async () => {
+    try {
+      // Rust側のdisconnect関数を呼び出す
+      await invoke("disconnect_serial_port");
+      console.log("Serial port disconnected successfully");
+      const consoleArea = document.getElementById("console");
+      consoleArea.value += "Serial port disconnected successfully\n";
+    } catch (error) {
+      console.error(`Error during disconnect: ${error}`);
+      const consoleArea = document.getElementById("console");
+      consoleArea.value += `Error during disconnect: ${error}\n`;
+    }
+  });
 
 // ファイル選択ダイアログを開く関数
 // function openFile() {
 //     const fileInput = document.getElementById('fileInput');
 //     fileInput.click(); // ユーザーがファイルを選択するためのダイアログを開く
 // }
+// Fileを開くイベント(ダイアログから取得したパスをバックエンドへ送る)
 document.getElementById("fileOpen").onclick = async () => {
-	console.log("click open");
-	const selected = await open({
-		multiple: false,
-		filters: [
-			{
-				name: "Midi",
-				extensions: ["mid"],
-			},
-			{
-				name: "All",
-				extensions: ["*"],
-			},
-		],
-	});
-	console.log("after dialog");
-	if (selected) {
-		let is_midi = await invoke("open_file", { path: selected });
-		let fname = selected.split("/").at(-1);
-		if (is_midi) {
-			document.getElementById("fname-display").innerHTML = fname;
-			displaySendButton();
-		} else {
-			// invalid file format
-			message(
-				`Invalid File Format. "${fname}" is not MIDI File Format.\nFullPath: ${selected}`,
-				{ title: "Error", type: "error" },
-			);
-		}
-	}
+  console.log("click open");
+  const selected = await open({
+    multiple: false,
+    filters: [
+      {
+        name: "Midi",
+        extensions: ["mid"],
+      },
+      {
+        name: "All",
+        extensions: ["*"],
+      },
+    ],
+  });
+  console.log("after dialog");
+  if (selected) {
+    let is_midi = await invoke("open_file", { path: selected });
+    let fname = selected.split("/").at(-1);
+    if (is_midi) {
+      document.getElementById("fname-display").innerHTML = fname;
+      displaySendButton();
+    } else {
+      // invalid file format
+      message(
+        `Invalid File Format. "${fname}" is not MIDI File Format.\nFullPath: ${selected}`,
+        { title: "Error", type: "error" },
+      );
+    }
+  }
 };
 // ファイルが選択されたときのイベントリスナー
 // document.getElementById('fileInput').addEventListener('change', async (event) => {
@@ -145,129 +146,129 @@ document.getElementById("fileOpen").onclick = async () => {
 
 // ファイルの内容をArrayBufferとして読み込む関数
 async function readFileAsArrayBuffer(file) {
-	return new Promise((resolve, reject) => {
-		const reader = new FileReader();
-		reader.onloadend = () => resolve(reader.result);
-		reader.onerror = reject;
-		reader.readAsArrayBuffer(file);
-	});
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result);
+    reader.onerror = reject;
+    reader.readAsArrayBuffer(file);
+  });
 }
 
 // ファイルサイズを表示する関数
 function displayFileSize(size) {
-	const fileSizeElemnt = document.getElementById("fileSize");
-	fileSizeElemnt.textContent = `ファイルサイズ: ${size} byte`;
+  const fileSizeElemnt = document.getElementById("fileSize");
+  fileSizeElemnt.textContent = `ファイルサイズ: ${size} byte`;
 }
 
 // 送信ボタンを表示する関数
 function displaySendButton() {
-	const sendButton = document.getElementById("sendButton");
-	// disabled を false にする
-	sendButton.disabled = false;
-	// cursor not-allowed を auto に
-	sendButton.style.cursor = "default";
-	// 背景色を青に
-	sendButton.style.backgroundColor = "#333";
+  const sendButton = document.getElementById("sendButton");
+  // disabled を false にする
+  sendButton.disabled = false;
+  // cursor not-allowed を auto に
+  sendButton.style.cursor = "default";
+  // 背景色を青に
+  sendButton.style.backgroundColor = "#333";
 }
 
 // 送信ボタンを無効にする関数
 function disableSendButton() {
-	const sendButton = document.getElementById("sendButton");
-	sendButton.disabled = true;
-	sendButton.style.cursor = "not-allowed";
-	sendButton.style.backgroundColor = "#aaa";
+  const sendButton = document.getElementById("sendButton");
+  sendButton.disabled = true;
+  sendButton.style.cursor = "not-allowed";
+  sendButton.style.backgroundColor = "#aaa";
 }
 
 // 警告メッセージを表示する関数
 function showWarningMessage() {
-	// html の #console textarea 内にエラーメッセージを表示
-	const consoleArea = document.getElementById("console");
-	// 自動で一番下までスクロール
-	consoleArea.scrollTop = consoleArea.scrollHeight;
-	// consoleArea 内にエラーメッセージを追加
-	consoleArea.value += "Error: MIDI形式のファイルを選択してください\n";
+  // html の #console textarea 内にエラーメッセージを表示
+  const consoleArea = document.getElementById("console");
+  // 自動で一番下までスクロール
+  consoleArea.scrollTop = consoleArea.scrollHeight;
+  // consoleArea 内にエラーメッセージを追加
+  consoleArea.value += "Error: MIDI形式のファイルを選択してください\n";
 }
 
 // 送信ボタンがクリックされたときのイベントリスナー
 document.getElementById("sendButton").addEventListener("click", async () => {
-	// const fileInput = document.getElementById('fileInput');
-	// const file = fileInput.files[0];
-	// const contents = await readFileAsArrayBuffer(file);
-	// console.log('Data send clicked');
+  // const fileInput = document.getElementById('fileInput');
+  // const file = fileInput.files[0];
+  // const contents = await readFileAsArrayBuffer(file);
+  // console.log('Data send clicked');
 
-	// イベントリスナーを設定
-	if (!playbackListenerId) {
-		window.__TAURI__.event
-			.listen("playback_info", (event) => {
-				const data = event.payload;
-				//console.log(`Received event: ${data}`);
-				const consoleArea = document.getElementById("console");
-				consoleArea.scrollTop = consoleArea.scrollHeight;
-				//consoleArea.value += `Playback Data: ${data}\n`;
+  // イベントリスナーを設定
+  if (!playbackListenerId) {
+    window.__TAURI__.event
+      .listen("playback_info", (event) => {
+        const data = event.payload;
+        //console.log(`Received event: ${data}`);
+        const consoleArea = document.getElementById("console");
+        consoleArea.scrollTop = consoleArea.scrollHeight;
+        //consoleArea.value += `Playback Data: ${data}\n`;
 
-				if (typeof data === "string") {
-					console.log(`String event received: ${data}`);
-					if (data.startsWith("Starting playback info")) {
-						console.log("Data sent successfully"); // デバッグ用ログ
-						switchPlayer();
-						updatePianoRoll(data);
-						// #playerConsole textarea デモ
-						const playerConsoleArea = document.getElementById("playerConsole");
-						playerConsoleArea.scrollTop = playerConsoleArea.scrollHeight;
-						playerConsoleArea.value += "==Switched Player==\n";
-					} else if (data.startsWith("Received response byte:")) {
-						// 受信したレスポンスバイトの処理
-						console.log(`Processing response: ${data}`);
-					} else if (data.startsWith("tempo:")) {
-						console.log(`Tempo event received: ${data}`);
-						// Tempoの処理
-					} else if (data.startsWith("chanel:")) {
-						console.log(`Channel event received: ${data}`);
-						// Channelイベントの処理
-					} else if (
-						data.startsWith("FlagA is invalid:") ||
-						data.startsWith("FlagA is 5: Skip to next track.")
-					) {
-						console.log(`Flag event received: ${data}`);
-						// Flagイベントの処理
-					} else {
-						console.warn(`Unknown event type: ${data}`);
-					}
-					// ピアノロールアップデート
-					// updatePianoRoll(data);
-				} else if (Array.isArray(data)) {
-					// console.log(`Array event received: ${JSON.stringify(data)}`);
-					// Arrayの処理（必要に応じて）
-					let dst = parse_event_msg(data[0], data[1], null);
-					if (dst !== null) {
-						if (dst.tempo !== 0) {
-							updateTempo(dst);
-						}
-						updatePianoRoll(dst);
-					}
-				} else {
-					console.warn(`Unexpected data type: ${typeof data}`);
-				}
-			})
-			.then((unlisten) => {
-				playbackListenerId = unlisten;
-			});
-	}
+        if (typeof data === "string") {
+          console.log(`String event received: ${data}`);
+          if (data.startsWith("Starting playback info")) {
+            console.log("Data sent successfully"); // デバッグ用ログ
+            switchPlayer();
+            updatePianoRoll(data);
+            // #playerConsole textarea デモ
+            const playerConsoleArea = document.getElementById("playerConsole");
+            playerConsoleArea.scrollTop = playerConsoleArea.scrollHeight;
+            playerConsoleArea.value += "==Switched Player==\n";
+          } else if (data.startsWith("Received response byte:")) {
+            // 受信したレスポンスバイトの処理
+            console.log(`Processing response: ${data}`);
+          } else if (data.startsWith("tempo:")) {
+            console.log(`Tempo event received: ${data}`);
+            // Tempoの処理
+          } else if (data.startsWith("chanel:")) {
+            console.log(`Channel event received: ${data}`);
+            // Channelイベントの処理
+          } else if (
+            data.startsWith("FlagA is invalid:") ||
+            data.startsWith("FlagA is 5: Skip to next track.")
+          ) {
+            console.log(`Flag event received: ${data}`);
+            // Flagイベントの処理
+          } else {
+            console.warn(`Unknown event type: ${data}`);
+          }
+          // ピアノロールアップデート
+          // updatePianoRoll(data);
+        } else if (Array.isArray(data)) {
+          // console.log(`Array event received: ${JSON.stringify(data)}`);
+          // Arrayの処理（必要に応じて）
+          let dst = parse_event_msg(data[0], data[1], null);
+          if (dst !== null) {
+            if (dst.tempo !== 0) {
+              updateTempo(dst);
+            }
+            updatePianoRoll(dst);
+          }
+        } else {
+          console.warn(`Unexpected data type: ${typeof data}`);
+        }
+      })
+      .then((unlisten) => {
+        playbackListenerId = unlisten;
+      });
+  }
 
-	try {
-		await invoke(tauriFunctionName); // Rust側のsend_file_sizeコマンドを呼び出し
-	} catch (error) {
-		message(`Error sending file size: ${error}`, {
-			title: "Error",
-			type: "error",
-		});
-		console.error(`Error sending file size: ${error}`); // エラーログ
+  try {
+    await invoke(tauriFunctionName); // Rust側のsend_file_sizeコマンドを呼び出し
+  } catch (error) {
+    message(`Error sending file size: ${error}`, {
+      title: "Error",
+      type: "error",
+    });
+    console.error(`Error sending file size: ${error}`); // エラーログ
 
-		// // #console textarea 内に送信失敗メッセージを表示
-		// const consoleArea = document.getElementById('console');
-		// // 自動で一番下までスクロール
-		// consoleArea.scrollTop = consoleArea.scrollHeight;
-		// // consoleArea 内にエラーメッセージを追加
-		// consoleArea.value += 'Error sending file size\n';
-	}
+    // // #console textarea 内に送信失敗メッセージを表示
+    // const consoleArea = document.getElementById('console');
+    // // 自動で一番下までスクロール
+    // consoleArea.scrollTop = consoleArea.scrollHeight;
+    // // consoleArea 内にエラーメッセージを追加
+    // consoleArea.value += 'Error sending file size\n';
+  }
 });
--- src/sequencer_msg_parser.js
+++ src/sequencer_msg_parser.js
@@ -6,103 +6,103 @@
  * @returns null | {ch: number, key: number, vel: number}
  */
 const [
-	EventKeyEvent,
-	EventTempo, // 4分音符１つ分のusec
-	EventEnd,
-	EventNop,
-	EventParam, // | ParamType(8bit) |slot(8bit)|data(8bit)|
-	EventProgramChange,
-	EventExpression,
-	EventText,
+  EventKeyEvent,
+  EventTempo, // 4分音符１つ分のusec
+  EventEnd,
+  EventNop,
+  EventParam, // | ParamType(8bit) |slot(8bit)|data(8bit)|
+  EventProgramChange,
+  EventExpression,
+  EventText,
 ] = [...Array(8)].map((_, i) => i);
 const PARAM_SET_LUT = [
-	"SlotMask",
-	"DetuneMultiple",
-	"TotalLevel",
-	"KeyScale AttackRate",
-	"DecayRate",
-	"SustainRate",
-	"SustainLevel ReleaseRate",
-	"FeedbackLevel Connection",
+  "SlotMask",
+  "DetuneMultiple",
+  "TotalLevel",
+  "KeyScale AttackRate",
+  "DecayRate",
+  "SustainRate",
+  "SustainLevel ReleaseRate",
+  "FeedbackLevel Connection",
 ];
 function convert_to_bpm(usec_per_beat) {
-	let minute_per_beat = usec_per_beat / 1000_000;
-	let beat_per_sec = 60 / minute_per_beat;
-	return usec_per_beat;
+  let minute_per_beat = usec_per_beat / 1000_000;
+  let beat_per_sec = 60 / minute_per_beat;
+  return usec_per_beat;
 }
 function parse_event_msg(lo, hi, print_log_func) {
-	let len = (lo & 0xf0) >>> 4;
-	if ((lo & 0x0f) != 1) {
-		// Todo: メッセージに合わせてよきに計らって。
-		console.log(`Other Message Received: Flag is ${lo & 0x0f}`);
-		return null;
-	}
-	let flag_a = (lo & 0xf00) >>> 8;
-	let ch = (lo & 0xf000) >>> 12;
-	let msg_data = lo >>> 16;
-	// Note: 未使用時0を記入
-	let dst = {
-		ch: ch, //Channel
-		key: 0, // Key
-		vel: 0, // Velocity
-		tempo: 0, // Tempo
-	};
-	if (!print_log_func) {
-		print_log_func = () => {};
-	}
-	switch (flag_a) {
-		case EventKeyEvent:
-			{
-				// KeyEvent
-				let key = msg_data & 0xff;
-				let vel = (msg_data & 0xff00) >>> 8;
-				dst.key = key;
-				dst.vel = vel;
-				print_log_func(`Ch${ch}: Key${vel ? "ON" : "OFF"} Key: ${key}`);
-			}
-			break;
-		case EventTempo:
-			{
-				let tempo = convert_to_bpm(msg_data + hi * 0x100);
-				print_log_func(`Tempo Change: ${tempo}`);
-				dst.tempo = tempo;
-			}
-			break;
-		case EventEnd:
-			print_log_func("End of playing");
-			dst = null;
-			break;
-		case EventParam:
-			{
-				let param_target = (msg_data >> 16) & 0xf;
-				let slot = (msg_data >>> 20) & 0xf;
-				let reg_value = (msg_data >>> 24) & 0xff;
-				print_log_func(
-					`Ch${ch}: Parameter Set @ slot${slot + 1} - ${PARAM_SET_LUT[param_target]} := ${reg_value}`,
-				);
-			}
-			dst = null;
-			break;
-		case EventProgramChange:
-			{
-				let name = String.fromCharCode(msg_data & 0x7f);
-				+String.fromCharCode((msg_data & 0x7f00) >> 8);
-				for (let i = 0; i < 4; i++) {
-					name += String.fromCharCode((hi >>> (i * 8)) & 0x7f);
-				}
-				print_log_func(`Ch${ch}: Preset Set "${name}"`);
-				dst = null;
-			}
-			break;
-		case EventExpression:
-			print_log_func(`Ch${ch}: Expression Change ${msg_data}`);
-			dst = null;
-			break;
-		default:
-			// console.log("hogehoge \(* ^  w  ^ *)/");
-			print_log_func(`Ch${ch}?: Something Event...`);
-			dst = null;
-			break;
-	}
-	return dst;
+  let len = (lo & 0xf0) >>> 4;
+  if ((lo & 0x0f) != 1) {
+    // Todo: メッセージに合わせてよきに計らって。
+    console.log(`Other Message Received: Flag is ${lo & 0x0f}`);
+    return null;
+  }
+  let flag_a = (lo & 0xf00) >>> 8;
+  let ch = (lo & 0xf000) >>> 12;
+  let msg_data = lo >>> 16;
+  // Note: 未使用時0を記入
+  let dst = {
+    ch: ch, //Channel
+    key: 0, // Key
+    vel: 0, // Velocity
+    tempo: 0, // Tempo
+  };
+  if (!print_log_func) {
+    print_log_func = () => {};
+  }
+  switch (flag_a) {
+    case EventKeyEvent:
+      {
+        // KeyEvent
+        let key = msg_data & 0xff;
+        let vel = (msg_data & 0xff00) >>> 8;
+        dst.key = key;
+        dst.vel = vel;
+        print_log_func(`Ch${ch}: Key${vel ? "ON" : "OFF"} Key: ${key}`);
+      }
+      break;
+    case EventTempo:
+      {
+        let tempo = convert_to_bpm(msg_data + hi * 0x100);
+        print_log_func(`Tempo Change: ${tempo}`);
+        dst.tempo = tempo;
+      }
+      break;
+    case EventEnd:
+      print_log_func("End of playing");
+      dst = null;
+      break;
+    case EventParam:
+      {
+        let param_target = (msg_data >> 16) & 0xf;
+        let slot = (msg_data >>> 20) & 0xf;
+        let reg_value = (msg_data >>> 24) & 0xff;
+        print_log_func(
+          `Ch${ch}: Parameter Set @ slot${slot + 1} - ${PARAM_SET_LUT[param_target]} := ${reg_value}`,
+        );
+      }
+      dst = null;
+      break;
+    case EventProgramChange:
+      {
+        let name = String.fromCharCode(msg_data & 0x7f);
+        +String.fromCharCode((msg_data & 0x7f00) >> 8);
+        for (let i = 0; i < 4; i++) {
+          name += String.fromCharCode((hi >>> (i * 8)) & 0x7f);
+        }
+        print_log_func(`Ch${ch}: Preset Set "${name}"`);
+        dst = null;
+      }
+      break;
+    case EventExpression:
+      print_log_func(`Ch${ch}: Expression Change ${msg_data}`);
+      dst = null;
+      break;
+    default:
+      // console.log("hogehoge \(* ^  w  ^ *)/");
+      print_log_func(`Ch${ch}?: Something Event...`);
+      dst = null;
+      break;
+  }
+  return dst;
 }
